4f41ab87a7c1e1ab72c05858fcc604c7723cce38
Rob Stewart <robjackstewart@gmail.com>
2026-03-25 20:58:47 +0000

    Title in navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    title: 'Dashboard',
     loadComponent: () =>
       import('./features/dashboard/dashboard').then(m => m.DashboardComponent)
   },
   {
     path: 'payment-sources',
+    title: 'Payment Sources',
     loadComponent: () =>
       import('./features/payment-sources/payment-source-list/payment-source-list').then(
         m => m.PaymentSourceListComponent
@@ -15,16 +17,19 @@ export const routes: Routes = [
   },
   {
     path: 'payees',
+    title: 'Payees',
     loadComponent: () =>
       import('./features/payees/payee-list/payee-list').then(m => m.PayeeListComponent)
   },
   {
     path: 'payments',
+    title: 'Payments',
     loadComponent: () =>
       import('./features/payments/payment-list/payment-list').then(m => m.PaymentListComponent)
   },
   {
     path: 'contacts',
+    title: 'Contacts',
     loadComponent: () =>
       import('./features/contacts/contact-list/contact-list').then(m => m.ContactListComponent)
   },
